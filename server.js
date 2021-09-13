e18d9e84dd941ec9681e589ccb928d6af8c53097
everglow <78077569+okchanho@users.noreply.github.com>
2021-09-13 19:57:34 +0900

    폼에 입력한 데이터를 서버에 전송하는 법 (POST요청)
    폼에 입력한 데이터를 서버에 전송하는 법 (POST요청)

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const app = express();
+//Json형태로 받아오기위해서
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.listen(8080, function (params) {
-    console.log('hello world');
+    console.log('start server');
 });
 
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html')
+});
+
+app.get('/write', function (req, res) {
+    res.sendFile(__dirname + '/write.html')
 });
 
 app.get('/pet', (req, res)=>{
@@ -16,5 +24,11 @@ app.get('/pet', (req, res)=>{
 
 app.get('/beauty', (req, res)=>{
     res.send('뷰티뷰티.');
+});
+
+app.post('/add', (req,res)=>{
+    console.log(req.body.title);
+    console.log(req.body.date);
+    res.send('전송완료');
 });
 
