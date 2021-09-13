2fd731b0c709a122e10212d40d6bfd8865200c0e
everglow <78077569+okchanho@users.noreply.github.com>
2021-09-13 21:35:32 +0900

    Database에 자료 저장하는 법 (한줄이면 끝)
    Database에 자료 저장하는 법 (한줄이면 끝)

--- server.js
+++ server.js
@@ -5,10 +5,17 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 //MongoDB
 const MongoClient = require('mongodb').MongoClient;
+var db; //변수 설정
 MongoClient.connect('mongodb+srv://root:koreait@cluster0.swgzj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',(err,client)=>{
+    if(err){console.log(err);}
+
+    db = client.db('Todoapp');
+    db.collection('post').insertOne({이름 : 'chanho', 나이: 20, _id: 100},function (err, result) {
+        console.log('저장완료');
+    });
     app.listen(8080, function () {
-    console.log('start server');
-});
+        console.log('start server');
+    });
 });
 
 
