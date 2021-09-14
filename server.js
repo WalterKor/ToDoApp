4167246cecdc6388322cf253bc9874fc22c73923
everglow <78077569+okchanho@users.noreply.github.com>
2021-09-14 11:01:55 +0900

    HTML에 DB데이터 꽂아넣는 법 1 (EJS)
    HTML에 DB데이터 꽂아넣는 법 1 (EJS)

--- server.js
+++ server.js
@@ -6,13 +6,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 //MongoDB
 const MongoClient = require('mongodb').MongoClient;
 var db; //변수 설정
+
+
+//set EJS
+app.set('view engine', 'ejs');
+
 MongoClient.connect('mongodb+srv://root:koreait@cluster0.swgzj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',(err,client)=>{
     if(err){console.log(err);}
 
     db = client.db('Todoapp');
-    db.collection('post').insertOne({이름 : 'chanho', 나이: 20, _id: 100},function (err, result) {
-        console.log('저장완료');
-    });
+    
     app.listen(8080, function () {
         console.log('start server');
     });
@@ -36,7 +39,15 @@ app.get('/beauty', (req, res)=>{
     res.send('뷰티뷰티.');
 });
 
-app.post('/add', (req,res)=>{
+app.get('/list', (req, res)=>{
+    res.render('list.ejs');
+});
+
+app.post('/add', (req,res)=>{    
+
+    db.collection('post').insertOne({제목 : req.body.title, 날짜: req.body.date },function (err, result) {
+        console.log('Post 저장완료');
+    });    
     console.log(req.body.title);
     console.log(req.body.date);
     res.send('전송완료');
