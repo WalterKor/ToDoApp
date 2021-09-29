fcdf7354d915b390564a9464eb04fd34c19317e0
everglow <78077569+okchanho@users.noreply.github.com>
2021-09-29 13:17:42 +0900

    (회원인증기능 1) 로그인 페이지 만들기 & 아이디 비번 검사

--- server.js
+++ server.js
@@ -92,4 +92,44 @@ app.put('/edit', function (req, res) {
         console.log('수정완료')
     })
     res.redirect('/list')
-})
+});
+
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+const passport = require('passport');
+
+app.use(session({
+    secret: '비밀코드',
+    resave: true,
+    saveUninitialized: false
+}));
+ app.use(passport.initialize());
+ app.use(passport.session());
+ 
+ app.get('/login', function (req, res) {
+     res.render('login.ejs')
+ })
+ app.post('/login', passport.authenticate('local', {
+    failureRedirect: '/fail'
+ }), function (req, res){
+    res.redirect('/')
+ });
+ 
+passport.use(new LocalStrategy({
+    usernameField: 'id',
+    passwordField: 'pw',
+    session: true, /*세션에 정보를 저장할것인지*/
+    passReqToCallback: false,
+}, function (입력한아이디, 입력한비번, done) {
+    //console.log(입력한아이디, 입력한비번);
+    db.collection('login').findOne({ id: 입력한아이디 }, function (에러, 결과) {
+    if (에러) return done(에러)
+
+    if (!결과) return done(null, false, { message: '존재하지않는 아이디요' })
+    if (입력한비번 == 결과.pw) {
+        return done(null, 결과)
+    } else {
+        return done(null, false, { message: '비번틀렸어요' })
+    }
+    })
+}));
